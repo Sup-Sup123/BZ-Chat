cc5d3dfe45f254c2200c690ac1aa8c187761d469
Sup-Sup123 <54416396+Sup-Sup123@users.noreply.github.com>
2020-08-28 22:31:26 -0500

    Add files via upload

---
+++ app/scripts/views/person.js
@@ -0,0 +1,319 @@
+/*
+ * Q-municate chat application
+ *
+ * Person Model
+ *
+ */
+define(["jquery", "underscore", "backbone", "config", "Helpers"], function(
+  $,
+  _,
+  Backbone,
+  QMCONFIG,
+  Helpers
+) {
+  var App;
+
+  var Person = Backbone.Model.extend({
+    defaults: {
+      full_name: null,
+      email: null,
+      password: "",
+      phone: "",
+      facebook_id: null,
+      is_provider: null,
+      avatar: null,
+      avatar_url: QMCONFIG.defAvatar.url,
+      status: "",
+      user_tags: null
+    },
+
+    validate: function(attrs) {
+      var MAX_SIZE = QMCONFIG.maxLimitFile * 1024 * 1024;
+
+      // Field: full_name
+      // mandatory; 3-200 characters;
+      if (!attrs.full_name) {
+        return "Name is required";
+      }
+      console.log(attrs.full_name);
+      console.log(attrs.full_name === "Unknown user");
+      if (attrs.full_name === "Unknown user") {
+        return QMCONFIG.errors.unknownUserName;
+      }
+      if (attrs.full_name.length < 3) {
+        return QMCONFIG.errors.shortName;
+      }
+      if (attrs.full_name.length > 200) {
+        return QMCONFIG.errors.bigName;
+      }
+
+      // Field: password
+      // mustn’t contain non-Latin characters and spaces; 8-40 characters
+      if (attrs.password) {
+        if (
+          !/^[A-Za-z0-9`~!@#%&=_<>;:,'\"\.\$\^\*\-\+\\\/\|\(\)\[\]\{\}\?]+$/.test(
+            attrs.password
+          )
+        ) {
+          return QMCONFIG.errors.invalidPass;
+        }
+        if (attrs.password.length < 8) {
+          return QMCONFIG.errors.shortPass;
+        }
+      }
+
+      // Field: avatar
+      // only image file; not more than 10 MB; filename not more than 100 characters
+      if (attrs.avatar) {
+        if (!/^image.{0,}$/.test(attrs.avatar.type)) {
+          return QMCONFIG.errors.avatarType;
+        }
+        if (attrs.avatar.size > MAX_SIZE) {
+          return QMCONFIG.errors.fileSize;
+        }
+        if (attrs.avatar.name.length > 100) {
+          return QMCONFIG.errors.fileName;
+        }
+      }
+    },
+
+    parse: function(data, options) {
+      if (typeof options === "object") {
+        App = options.app;
+      }
+
+      _.each(
+        data,
+        function(val, key) {
+          var isHasKey = _.has(this.defaults, key);
+          if (key !== "id" && !isHasKey) {
+            delete data[key];
+          } else if (typeof val === "string") {
+            data[key] = val.trim();
+          }
+        },
+        this
+      );
+
+      return data;
+    },
+
+    initialize: function() {},
+
+    update: function() {
+      var currentUser = App.models.User.contact,
+        QBApiCalls = App.service,
+        data = this.toJSON(),
+        params = {},
+        custom_data =
+          (currentUser.custom_data && JSON.parse(currentUser.custom_data)) ||
+          {},
+        self = this;
+
+      if (
+        Object.keys(data).length === 0 ||
+        (Object.keys(data).length === 1 &&
+          Object.keys(data)[0] === "avatar" &&
+          !data.avatar)
+      )
+        return;
+
+      if (data.full_name) {
+        params.full_name = currentUser.full_name = data.full_name;
+      }
+      if (data.phone) {
+        params.phone = currentUser.phone = data.phone;
+      }
+      if (data.status.length >= 0) {
+        custom_data.status = currentUser.status = data.status;
+        params.custom_data = currentUser.custom_data = JSON.stringify(
+          custom_data
+        );
+      }
+      if (data.avatar) {
+        this.uploadAvatar(data.avatar, function(blob) {
+          var avatarUrl = QB.content.publicUrl(blob.uid);
+
+          self.set("avatar_url", avatarUrl);
+
+          params.blob_id = currentUser.blob_id = blob.id;
+          custom_data.avatar_url = currentUser.avatar_url = avatarUrl;
+          params.custom_data = currentUser.custom_data = JSON.stringify(
+            custom_data
+          );
+
+          $('.profileUserName[data-id="' + currentUser.id + '"]').text(
+            currentUser.full_name
+          );
+          $('.profileUserAvatar[data-id="' + currentUser.id + '"]').css(
+            "background-image",
+            "url(" + currentUser.avatar_url + ")"
+          );
+          App.models.User.rememberMe();
+
+          QBApiCalls.updateUser(currentUser.id, params, function(res) {
+            Helpers.log("update of user", res);
+          });
+        });
+      } else {
+        $('.profileUserName[data-id="' + currentUser.id + '"]').text(
+          currentUser.full_name
+        );
+        App.models.User.rememberMe();
+
+        QBApiCalls.updateUser(currentUser.id, params, function(res) {
+          Helpers.log("update of user", res);
+        });
+      }
+    },
+
+    uploadAvatar: function(avatar, callback) {
+      var QBApiCalls = App.service,
+        Attach = App.models.Attach;
+
+      Attach.crop(
+        avatar,
+        {
+          w: 1000,
+          h: 1000
+        },
+        function(file) {
+          QBApiCalls.createBlob(
+            {
+              file: file,
+              public: true
+            },
+            function(blob) {
+              callback(blob);
+            }
+          );
+        }
+      );
+    },
+
+    changeQBPass: function(data, callback) {
+      var currentUser = App.models.User.contact,
+        Session = App.models.Session,
+        QBApiCalls = App.service,
+        params = {},
+        self = this;
+
+      params.old_password = data.oldPass;
+      params.password = data.newPass;
+
+      QBApiCalls.updateUser(currentUser.id, params, function(res, err) {
+        if (res) {
+          Helpers.log("update of user", res);
+          Session.update(
+            {
+              authParams: Session.encrypt({
+                email: currentUser.email,
+                password: params.password
+              })
+            },
+            true
+          );
+          self.set("password", "");
+          callback(null, res);
+        } else {
+          callback(err, null);
+        }
+      });
+    },
+
+    connectFB: function(fbId, callback) {
+      var currentUser = App.models.User.contact,
+        QBApiCalls = App.service,
+        custom_data =
+          (currentUser.custom_data && JSON.parse(currentUser.custom_data)) ||
+          {},
+        params = {},
+        self = this;
+
+      if (self.get("avatar_url") === QMCONFIG.defAvatar.url) {
+        custom_data.avatar_url =
+          "https://graph.facebook.com/v3.0/" +
+          fbId +
+          "/picture?width=1200&height=1200";
+      }
+      custom_data.is_import = "1";
+      params.facebook_id = fbId;
+      params.custom_data = JSON.stringify(custom_data);
+
+      QBApiCalls.updateUser(currentUser.id, params, function(res, err) {
+        if (res) {
+          Helpers.log("update of user", res);
+
+          if (self.get("avatar_url") === QMCONFIG.defAvatar.url) {
+            self.set("avatar_url", custom_data.avatar_url);
+            currentUser.avatar_url = custom_data.avatar_url;
+          }
+          self.set("facebook_id", fbId);
+          currentUser.facebook_id = fbId;
+          currentUser.custom_data = params.custom_data;
+
+          $('.profileUserAvatar[data-id="' + currentUser.id + '"]').css(
+            "background-image",
+            "url(" + currentUser.avatar_url + ")"
+          );
+          App.models.User.rememberMe();
+
+          // import friends
+          self.getFBFriends();
+
+          callback(null, res);
+        } else {
+          callback(err, null);
+        }
+      });
+    },
+
+    getFBFriends: function() {
+      var isFriendsPermission = false,
+        self = this;
+
+      FB.api("/me/permissions", function(response) {
+        Helpers.log("FB Permissions", response);
+        for (var i = 0, len = response.data.length; i < len; i++) {
+          if (
+            response.data[i].permission === "user_friends" &&
+            response.data[i].status === "granted"
+          ) {
+            isFriendsPermission = true;
+          }
+        }
+
+        if (isFriendsPermission) {
+          // import FB friends
+          FB.api("/me/friends", function(res) {
+            Helpers.log("FB friends", res);
+            var ids = [];
+
+            for (var i = 0, len = res.data.length; i < len; i++) {
+              ids.push(res.data[i].id);
+            }
+
+            if (ids.length > 0) {
+              self.import(ids);
+            }
+          });
+        }
+      });
+    },
+
+    import: function(ids) {
+      var ContactList = App.models.ContactList,
+        ContactListView = App.views.ContactList,
+        FBImport = App.views.FBImport;
+
+      ContactList.getFBFriends(ids, function(new_ids) {
+        for (var i = 0, len = new_ids.length; i < len; i++) {
+          ContactListView.importFBFriend(new_ids[i]);
+        }
+        FBImport.render().openPopup();
+      });
+    }
+  });
+
+  return Person;
+});
---
+++ app/scripts/views/videochat.js
@@ -0,0 +1,190 @@
+/*
+ * Q-municate chat application
+ *
+ * VideoChat Module
+ *
+ */
+define(["jquery", "config", "Helpers"], function($, QMCONFIG, Helpers) {
+  var curSession, self;
+
+  function VideoChat(app) {
+    this.app = app;
+    this.session = {};
+    self = this;
+  }
+
+  VideoChat.prototype.getUserMedia = function(options, callType, callback) {
+    var User = this.app.models.User;
+    var params = {
+      audio: true,
+      video: callType === "video" ? true : false,
+      elemId: "localStream",
+      options: {
+        muted: true,
+        mirror: true
+      }
+    };
+
+    if (!options.isCallee) {
+      if (callType === "video") {
+        self.session = QB.webrtc.createNewSession(
+          [options.opponentId],
+          QB.webrtc.CallType.VIDEO,
+          null,
+          { bandwidth: 512 }
+        );
+      } else {
+        self.session = QB.webrtc.createNewSession(
+          [options.opponentId],
+          QB.webrtc.CallType.AUDIO
+        );
+      }
+    }
+
+    curSession = self.session;
+
+    curSession.getUserMedia(params, function(err, stream) {
+      if (err) {
+        Helpers.log("Error", err);
+        if (!options.isCallee) {
+          callback(err, null);
+        } else {
+          self.sendMessage(
+            options.opponentId,
+            "3",
+            null,
+            options.dialogId,
+            callType,
+            true
+          );
+          callback(err, null);
+        }
+      } else {
+        Helpers.log("Stream", stream);
+
+        if (
+          !$('.l-chat[data-dialog="' + options.dialogId + '"]').find(
+            ".mediacall"
+          )[0]
+        ) {
+          stream.stop({});
+          return true;
+        }
+
+        if (options.isCallee) {
+          curSession.accept({});
+          self.caller = options.opponentId;
+          self.callee = User.contact.id;
+        } else {
+          curSession.call({});
+          self.caller = User.contact.id;
+          self.callee = options.opponentId;
+        }
+        callback(null, stream);
+      }
+    });
+  };
+
+  VideoChat.prototype.sendMessage = function(
+    userId,
+    state,
+    callDuration,
+    dialogId,
+    callType,
+    isErrorMessage,
+    sessionID
+  ) {
+    var jid = QB.chat.helpers.getUserJid(userId, QMCONFIG.qbAccount.appId),
+      User = this.app.models.User,
+      Message = this.app.models.Message,
+      MessageView = this.app.views.Message,
+      VideoChatView = this.app.views.VideoChat,
+      DialogView = this.app.views.Dialog,
+      time = Math.floor(Date.now() / 1000),
+      $dialogItem = $(
+        '.l-list-wrap section:not(#searchList) .dialog-item[data-dialog="' +
+          dialogId +
+          '"]'
+      ),
+      selected = $("[data-dialog = " + dialogId + "]").is(".is-selected"),
+      unread = parseInt(
+        $dialogItem.length > 0 && $dialogItem.find(".unread").text().length > 0
+          ? $dialogItem.find(".unread").text()
+          : 0
+      ),
+      extension,
+      message,
+      msg;
+
+    if (!isErrorMessage) {
+      extension = {
+        save_to_history: 1,
+        date_sent: time,
+        callType:
+          state === "2"
+            ? callType === "video"
+              ? "2"
+              : "1"
+            : VideoChatView.type === "video"
+            ? "2"
+            : "1",
+        callState: state === "1" && !callDuration ? "2" : state,
+        caller: state === "2" ? userId : self.caller,
+        callee: state === "2" ? User.contact.id : self.callee
+      };
+
+      if (callDuration)
+        extension.callDuration = Helpers.getDuration(null, callDuration);
+    } else {
+      extension = {
+        save_to_history: 1,
+        date_sent: time,
+        callType: callType === "video" ? "2" : "1",
+        callState: state,
+        caller: userId,
+        callee: User.contact.id
+      };
+    }
+
+    if (sessionID) {
+      extension.sessionID = sessionID;
+    }
+
+    msg = {
+      chat_dialog_id: dialogId,
+      date_sent: time,
+      sender_id: User.contact.id,
+      callType: extension.callType,
+      callState: extension.callState,
+      caller: extension.caller,
+      callee: extension.callee,
+      callDuration: extension.callDuration || null,
+      sessionID: extension.sessionID || null,
+      online: true
+    };
+
+    msg.id = QB.chat.send(jid, {
+      type: "chat",
+      body: "Call notification",
+      extension: extension
+    });
+
+    message = Message.create(msg);
+    Helpers.log(message);
+    MessageView.addItem(message, true, true);
+
+    // show counter on dialog item about missed calls
+    if (!selected) {
+      unread++;
+      $dialogItem.find(".unread").text(unread);
+      DialogView.getUnreadCounter(dialogId);
+    }
+
+    Helpers.Dialogs.moveDialogToTop(dialogId);
+  };
+
+  /* Private
+    ---------------------------------------------------------------------- */
+
+  return VideoChat;
+});
